c7648936dc6af5b5406c1760635f306eda2da157
zhangcong <zhangcong@9linktech.com>
2023-05-26 11:28:17 +0800

    chore:更改图片文件目录

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const path = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.useStaticAssets(path.join(__dirname, '..', 'public'));
+    app.useStaticAssets('/images', { prefix: '/images' });
     await app.listen(3000);
 }
 bootstrap().catch(console.error);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import type { NestExpressApplication } from '@nestjs/platform-express'
-import * as path from 'path'
+// import * as path from 'path'
 
 async function bootstrap () {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
-  app.useStaticAssets(path.join(__dirname, '..', 'public'))
+  app.useStaticAssets('/images', { prefix: '/images' })
   await app.listen(3000)
 }
 
