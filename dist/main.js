6d1026833ad2e6673797ba526447524265ef6082
zhangcong <zhangcong@9linktech.com>
2023-05-26 12:07:32 +0800

    chore: 增加证书 开启https

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const node_fs_1 = require("node:fs");
+const httpsOptions = {
+    key: node_fs_1.default.readFileSync('/usr/local/nginx/cert/chasingdream.cn.pem'),
+    cert: node_fs_1.default.readFileSync('/usr/local/nginx/cert/chasingdream.cn.key')
+};
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        httpsOptions
+    });
     app.useStaticAssets('/images', { prefix: '/images' });
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import type { NestExpressApplication } from '@nestjs/platform-express'
+import fs from 'node:fs'
 // import * as path from 'path'
-
+const httpsOptions = {
+  key: fs.readFileSync('/usr/local/nginx/cert/chasingdream.cn.pem'),
+  cert: fs.readFileSync('/usr/local/nginx/cert/chasingdream.cn.key')
+}
 async function bootstrap () {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions
+  })
 
   app.useStaticAssets('/images', { prefix: '/images' })
   await app.listen(3000)
